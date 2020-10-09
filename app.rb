6b6d4ac9a9fe63a2038a12051aaca839d401a883
jurij <yura19802007@gmail.com>
2020-10-09 18:28:27 +0300

    Create database on initialize

--- app.rb
+++ app.rb
@@ -3,6 +3,21 @@ require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
 #require 'pony'
+require 'sqlite3'
+
+configure do
+	@db = SQLite3::Database.new 'barbershop.db'
+	@db.execute 'CREATE TABLE IF NOT EXISTS
+	"users"
+	(
+		"id" INTEGER PRIMARY KEY,
+		"username" TEXT,
+		"datestamp" TEXT,
+		"barber" TEXT,
+		"color" TEXT
+	
+	)'
+end
 
 get '/' do
 	
